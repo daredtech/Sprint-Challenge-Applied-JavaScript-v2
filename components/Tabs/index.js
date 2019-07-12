1b73e5634e129a045569e239de19a1c2b87190d8
Red <vhanchar@ucsc.edu>
2019-07-12 09:12:35 -0700

    gets the collection, but skips to catch

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -9,9 +9,28 @@
 //    <div class="tab">topic here</div>
 
 
+
 //to request information from the server
 axios.get('https://lambda-times-backend.herokuapp.com/topics')
 //if successful
-.then(data => {console.log(data)})
+.then(data => {console.log(data.data); let topicCollection = data.data.topics; console.log("MY TOPIC COLLECTION IS: " + topicCollection);topicCollection.forEach(element => {createTabs(element)}); })
+// .then(data => {console.log(data.data); console.log("MY FIRST ELEMENT OF THE ARRAY :" + data.data.topics[0]); })
+
+
 //if not successful
 .catch(error => {console.log('Unable to return complete GET request')})
+
+
+const topics = document.querySelector('.topics');
+
+function createTabs(topicText){
+    //define the element
+    const tab = document.createElement(div);
+    //setup the structure
+    topics.addChild(tab);
+    //setup the styling
+    tab.classList.add('tab');
+    //setup the content
+    tab.textContent = topicText;
+    return tab;
+}
